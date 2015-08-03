b556821b9150d64c40be87560f002fbe110fd989
Tyler Machen <tylermachen@gmail.com>
2015-08-02 22:22:00 -0400

    complete pigeon organizer

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+
+  data[:gender].values.flatten.each do |name|
+    pigeons[name] = {}
+
+    data.each do |feature, options|
+      pigeons[name][feature] = []
+
+      options.each do |option, names|
+        pigeons[name][feature] << option.to_s if names.include?(name)
+      end
+    end
+  end
+  
+  pigeons
 end
